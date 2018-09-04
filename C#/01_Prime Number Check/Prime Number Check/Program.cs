8e903757ac51bb3c566bfa79dfb0b407cace820f
Cliff Su <stu01509@yahoo.com.tw>
2018-09-04 21:06:10 +0800

    ⌨️ Add Files

---
+++ C#/01_Prime Number Check/Prime Number Check/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Number_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, i, k;
+            k = 0;
+            Console.WriteLine("Prime Number Check 質數判斷\r\n");
+            Console.WriteLine("請輸入一個數字.");
+            a = Int32.Parse(Console.ReadLine());
+
+            for (i = 1; i <= a; i++)
+            {
+                if (a % i == 0)
+                {
+                    k++;
+                }
+            }
+
+            if (k == 2)
+            {
+                Console.WriteLine("Yes.");
+            }
+            else
+            {
+                Console.WriteLine("No.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/02_Standard Body Weight Calc/02_Standard Body Weight Calc_1/Standard Body Weight Calc/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Standard_Body_Weight_Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String inputHeight, inputSex,male,female;
+            int int_inputHeight, int_inputSex ;
+            double double_male, double_female;
+            Console.WriteLine("標準體重計算\r\n");
+            Console.WriteLine("請輸入身高.");
+            inputHeight = Console.ReadLine();
+            Console.WriteLine("請輸入性別.");
+            inputSex = Console.ReadLine();
+
+            int_inputHeight = Convert.ToInt32(inputHeight);
+            int_inputSex = Convert.ToInt32(inputSex);
+
+            if(int_inputSex== 1)
+            {
+                double_male = (int_inputHeight - 80) *0.7;
+                male = double_male.ToString("f1");
+                Console.WriteLine(male);
+            }
+            else if(int_inputSex == 2){
+                double_female = (int_inputHeight - 70) * 0.6;
+                female = double_female.ToString("f1");
+                Console.WriteLine(female);
+            }
+            else
+            {
+                Console.WriteLine("請輸入正確性別喔 Bye Bye");
+            }
+            Console.ReadLine();
+        }
+    }
+}
