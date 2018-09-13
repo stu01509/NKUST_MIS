64b3971736b6d4d8e68172840467665db7c8c6c5
Cliff Su <stu01509@yahoo.com.tw>
2018-09-14 06:15:38 +0800

    🛠️ Fix C# 04

--- C#/04_Electricity Pricing Calc/Electricity Pricing Calc/Program.cs
+++ C#/04_Electricity Pricing Calc/Electricity Pricing Calc/Program.cs
@@ -13,7 +13,7 @@ namespace Electricity_Pricing_Calc
             String inputWatt ;
             int int_inputWatt;
             double[] summer = {2.10, 3.02, 4.39, 4.97, 5.63 };
-            double[] nosummer = { 2.10, 3.02, 4.39, 4.97, 5.63 };
+            double[] nosummer = {2.10, 2.68, 3.61, 4.01, 4.50 };
 
 
             Console.WriteLine("電費計算");
@@ -25,27 +25,27 @@ namespace Electricity_Pricing_Calc
             if (int_inputWatt < 120)
             {
                 Console.WriteLine("Summer months:" + Math.Round(int_inputWatt * summer[0], 2));
-                Console.WriteLine("Summer months:" + Math.Round(int_inputWatt * nosummer[0], 2));
+                Console.WriteLine("Non-Summer months:" + Math.Round(int_inputWatt * nosummer[0], 2));
             }
             else if( int_inputWatt<=330)
             {
                 Console.WriteLine("Summer months:" + Math.Round(int_inputWatt * summer[1], 2));
-                Console.WriteLine("Summer months:" + Math.Round(int_inputWatt * nosummer[1], 2));
+                Console.WriteLine("Non-Summer months:" + Math.Round(int_inputWatt * nosummer[1], 2));
             }
             else if (int_inputWatt <= 500)
             {
                 Console.WriteLine("Summer months:" + Math.Round(int_inputWatt * summer[2], 2));
-                Console.WriteLine("Summer months:" + Math.Round(int_inputWatt * nosummer[2], 2));
+                Console.WriteLine("Non-Summer months:" + Math.Round(int_inputWatt * nosummer[2], 2));
             }
             else if (int_inputWatt <= 700)
             {
                 Console.WriteLine("Summer months:" + Math.Round(int_inputWatt * summer[3], 2));
-                Console.WriteLine("Summer months:" + Math.Round(int_inputWatt * nosummer[3], 2));
+                Console.WriteLine("Non-Summer months:" + Math.Round(int_inputWatt * nosummer[3], 2));
             }
             else
             {
                 Console.WriteLine("Summer months:" + Math.Round(int_inputWatt * summer[4], 2));
-                Console.WriteLine("Summer months:" + Math.Round(int_inputWatt * nosummer[4], 2));
+                Console.WriteLine("Non-Summer months:" + Math.Round(int_inputWatt * nosummer[4], 2));
             }
             Console.ReadLine();
         }
