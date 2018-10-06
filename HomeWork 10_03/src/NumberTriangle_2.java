dce8d13c21292ecd7854b2af84c951cc1ab3b736
Cliff Su <stu01509@yahoo.com.tw>
2018-10-06 09:10:39 +0800

    Add HomeWork 10_03

---
+++ HomeWork 10_03/src/NumberTriangle_2.java
@@ -0,0 +1,16 @@
+public class NumberTriangle_2{
+
+  public static void main(String[] args){
+
+    for(int i = 5; i >= 1; i--){
+
+      for(int j = 5; j >= i; j--){
+        System.out.print(j);
+      }
+
+      System.out.println();
+    }
+
+  }
+
+}
