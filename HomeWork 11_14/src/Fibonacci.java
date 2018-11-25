6634dfc1ebc0907f44452836ad7eadd14bd11ec1
Cliff Su <stu01509@yahoo.com.tw>
2018-11-26 06:58:36 +0800

    Add HomeWork 11_14

---
+++ HomeWork 11_14/src/Fibonacci.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		
+		System.out.println("M16. 費氏數列");
+
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("請輸入第 n 個費氏數列的值: ");
+			
+		int inputN = Integer.valueOf(scanner.nextLine());
+			
+		System.out.println("費氏數列的總值 fib(" + inputN + ") = " + fib(inputN));
+		
+	}
+	
+	public static long fib(int n) {
+		
+		if (n == 1 || n== 2) {
+			return 1;
+		} else {
+			return fib(n - 1) + fib(n - 2);
+		}
+		
+	}
+
+}
